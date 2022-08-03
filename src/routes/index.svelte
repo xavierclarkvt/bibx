696424b2b86ed16735a97c02e5244d9a389ef1a6
Xavier Clark <xavier@clar.cx>
2022-08-02 22:02:42 -0400

    Added counter to homepage Added svelte-kit static adapter / build config

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,14 @@
+<script>
+	let count = 0;
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<h1>
+	{count}
+</h1>
+
+<button on:click={() => count++}>+</button>
+<button on:click={() => count--}>-</button>
+<button on:click={() => (count = 0)}>reset</button>
